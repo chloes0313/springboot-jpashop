4045cd2a9a81d2d8fbcc70f8707467c6b25e50ad
chloeseong <chloeseong@mnwise.com>
2023-10-12 15:43:56 +0900

    chap#02 domain chap#03 entity

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer orderPrice;
+
+    private Integer count;
+}
